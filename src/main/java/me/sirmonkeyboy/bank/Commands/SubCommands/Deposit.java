cdae3edcc6f01c1f4b1f1b6bdc3ed76893b76b1c
SirMonkeyboy <116051515+SirMonkeyBoy@users.noreply.github.com>
2023-09-28 18:53:24 +1000

    Adds Deposit command fully

--- src/main/java/me/sirmonkeyboy/bank/Commands/SubCommands/Deposit.java
+++ src/main/java/me/sirmonkeyboy/bank/Commands/SubCommands/Deposit.java
@@ -8,10 +8,17 @@ import org.bukkit.entity.Player;
 import net.milkbowl.vault.economy.Economy;
 
 import java.util.List;
+import java.util.Objects;
 
 import static org.bukkit.ChatColor.translateAlternateColorCodes;
 
 public class Deposit extends SubCommand {
+
+    private Bank plugin;
+
+    public Deposit(Bank plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public String getName() {
@@ -33,24 +40,32 @@ public class Deposit extends SubCommand {
         if (p.hasPermission("Bank.commands.Bank.Deposit")) {
             Economy eco = Bank.getEconomy();
             try {
-                int DepositMinimum = 1000;
+                int DepositMinimum = Integer.parseInt(Objects.requireNonNull(plugin.getConfig().getString("MinimumAmount")));
                 int DepositAmount = Integer.parseInt(args[1]);
                 if (DepositAmount >= DepositMinimum){
-                    if (DepositAmount <= eco.getBalance(p)){
-                        p.sendMessage("Deposited $" + DepositAmount);
+                    String DepositMessage = plugin.getConfig().getString("DepositMessage");
+                    if (DepositAmount <= eco.getBalance(p)) {
+                        if (DepositMessage != null){
+                            eco.withdrawPlayer(p, DepositAmount);
+                            plugin.data.addbalance(p.getUniqueId(), DepositAmount);
+                            p.sendMessage(translateAlternateColorCodes('&',DepositMessage + DepositAmount));
+                        }
                     }else {
-                        p.sendMessage("You Don't have $" + DepositAmount + " in your balance");
+                        p.sendMessage(translateAlternateColorCodes('&',"You don't have $" + DepositAmount + " in your balance"));
                     }
                 }
                 else {
-                    p.sendMessage("Minimum deposit amount is $1000");
+                    p.sendMessage(translateAlternateColorCodes('&',"Minimum deposit amount is $1000"));
                 }
             }catch (NumberFormatException e){
-                p.sendMessage("Minimum deposit amount is $1000");
+                p.sendMessage("Please deposit a number");
             }
         } else {
             if (!p.hasPermission("Bank.commands.Bank.Deposit")) {
-                p.sendMessage(translateAlternateColorCodes('&', "&cYou don't have permission to use this command"));
+                String noPermission = plugin.getConfig().getString("NoPermission");
+                if (noPermission != null) {
+                    p.sendMessage(translateAlternateColorCodes('&', noPermission));
+                }
             }
         }
     }
