78d26862a767515c56997d99d93bc1b2095baa6c
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2024-10-25 14:28:17 +1100

    Update Withdraw.java

--- src/main/java/me/sirmonkeyboy/bank/Commands/SubCommands/Withdraw.java
+++ src/main/java/me/sirmonkeyboy/bank/Commands/SubCommands/Withdraw.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Objects;
 
-import static org.bukkit.ChatColor.translateAlternateColorCodes;
-
 public class Withdraw extends SubCommand {
 
     private final Bank plugin;
@@ -42,48 +40,47 @@ public class Withdraw extends SubCommand {
     public void perform(Player p, String[] args) {
         if (p.hasPermission("Bank.commands.Bank.Withdraw")) {
             Economy eco = Bank.getEconomy();
+
             try {
-                int WithdrawMinimum = Integer.parseInt(Objects.requireNonNull(plugin.getConfig().getString("MinimumAmount")));
-                double WithdrawAmount = Double.parseDouble(args[1]);
-                String WithdrawAmountStr = String.valueOf(WithdrawAmount);
-                String WithdrawMessage = plugin.getConfig().getString("Withdraw.WithdrawMessage");
-                String DontHaveEnoughInBalance = plugin.getConfig().getString("Withdraw.DontHaveEnoughInBalance");
-                String MinimumWithdrawMessage = plugin.getConfig().getString("Withdraw.MinimumWithdrawMessage");
-                String MinimumWithdrawAmount = String.valueOf(WithdrawMinimum);
-                if (WithdrawAmount >= WithdrawMinimum){
-                    if (WithdrawAmount == WithdrawMinimum) {
-                        if (WithdrawMessage != null){
-                            eco.depositPlayer(p, WithdrawAmount);
-                            plugin.data.rembalance(p.getUniqueId(), WithdrawAmount);
-                            WithdrawMessage = WithdrawMessage.replace("%Withdraw%", WithdrawAmountStr);
-                            p.sendMessage(Component.text(WithdrawMessage).color(NamedTextColor.GREEN));
-                        }
-                    }else {
-                        if (DontHaveEnoughInBalance != null) {
+                if (args.length >= 2 && !args[1].isBlank()) {
+                    int WithdrawMinimum = Integer.parseInt(Objects.requireNonNull(this.plugin.getConfig().getString("MinimumAmount")));
+                    double WithdrawAmount = Double.parseDouble(args[1]);
+                    String WithdrawAmountStr = String.valueOf(WithdrawAmount);
+                    String WithdrawMessage = this.plugin.getConfig().getString("Withdraw.WithdrawMessage");
+                    String DontHaveEnoughInBalance = this.plugin.getConfig().getString("Withdraw.DontHaveEnoughInBalance");
+                    String MinimumWithdrawMessage = this.plugin.getConfig().getString("Withdraw.MinimumWithdrawMessage");
+                    String MinimumWithdrawAmount = String.valueOf(WithdrawMinimum);
+                    if (WithdrawAmount >= WithdrawMinimum) {
+                        if (WithdrawAmount == WithdrawMinimum) {
+                            if (WithdrawMessage != null) {
+                                eco.depositPlayer(p, WithdrawAmount);
+                                this.plugin.data.rembalance(p.getUniqueId(), WithdrawAmount);
+                                WithdrawMessage = WithdrawMessage.replace("%Withdraw%", WithdrawAmountStr);
+                                p.sendMessage(Component.text(WithdrawMessage).color(NamedTextColor.GREEN));
+                            }
+                        } else if (DontHaveEnoughInBalance != null) {
                             DontHaveEnoughInBalance = DontHaveEnoughInBalance.replace("%Deposit%", WithdrawAmountStr);
                             p.sendMessage(Component.text(DontHaveEnoughInBalance).color(NamedTextColor.RED));
                         }
-                    }
-                }
-                else {
-                    if (MinimumWithdrawMessage != null) {
+                    } else if (MinimumWithdrawMessage != null) {
                         MinimumWithdrawMessage = MinimumWithdrawMessage.replace("%Minimum%", MinimumWithdrawAmount);
                         p.sendMessage(Component.text(MinimumWithdrawMessage).color(NamedTextColor.RED));
                     }
+                } else {
+                    p.sendMessage(Component.text("Use /bank withdraw Amount"));
                 }
-            }catch (NumberFormatException e){
+            } catch (NumberFormatException var12) {
                 p.sendMessage(Component.text("Please enter a number").color(NamedTextColor.RED));
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
-        } else {
-            if (!p.hasPermission("Bank.commands.Bank.Withdraw")) {
-                String noPermission = plugin.getConfig().getString("NoPermission");
-                if (noPermission != null) {
-                    p.sendMessage(Component.text(noPermission).color(NamedTextColor.RED));
-                }
+        } else if (!p.hasPermission("Bank.commands.Bank.Withdraw")) {
+            String noPermission = this.plugin.getConfig().getString("NoPermission");
+            if (noPermission != null) {
+                p.sendMessage(Component.text(noPermission).color(NamedTextColor.RED));
             }
         }
+
     }
 
     @Override
