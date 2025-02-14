b2d7245cdb7a427fc2997feaa40f066f5deb095c
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-02-14 16:38:04 +1100

    Update Withdraw SQL

--- src/main/java/me/sirmonkeyboy/bank/Utill/MariaDB.java
+++ src/main/java/me/sirmonkeyboy/bank/Utill/MariaDB.java
@@ -208,12 +208,12 @@ public class MariaDB {
         }
     }
 
-    public void WithdrawTransaction(UUID uuid, String name, double amount) throws SQLException {
+    public void WithdrawTransaction(UUID uuid, String name, double amount, double newbalance) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false", username, password);
 
         try {
             conn.setAutoCommit(false);
-            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO transactions (username, uuid, time, type, amount) VALUES (?, ?, ?, ?, ?)");
+            PreparedStatement pstmt = conn.prepareStatement("INSERT INTO transactions (username, uuid, time, type, amount, newbalance) VALUES (?, ?, ?, ?, ?, ?)");
             String type = "WITHDRAW";
             long currentTimeMillis = System.currentTimeMillis();
             java.sql.Timestamp timestamp = new java.sql.Timestamp(currentTimeMillis);
@@ -222,6 +222,7 @@ public class MariaDB {
             pstmt.setTimestamp(3, timestamp);
             pstmt.setString(4, type);
             pstmt.setDouble(5, amount);
+            pstmt.setDouble(6, newbalance);
             pstmt.executeUpdate();
             conn.commit();
         }catch (SQLException e) {
