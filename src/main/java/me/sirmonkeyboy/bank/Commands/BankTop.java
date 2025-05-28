3541f7e0ab1f85eebda40ee3499a1ddcd1da377b
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-05-28 21:00:37 +1000

    Create BankTop.java

---
+++ src/main/java/me/sirmonkeyboy/bank/Commands/BankTop.java
@@ -0,0 +1,56 @@
+package me.sirmonkeyboy.bank.Commands;
+
+import me.sirmonkeyboy.bank.Bank;
+import me.sirmonkeyboy.bank.Utill.MariaDB;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class BankTop implements TabExecutor {
+
+    private final Bank plugin;
+
+    private final MariaDB data;
+
+    public BankTop(Bank plugin) {
+        this.plugin = plugin;
+        this.data = plugin.data;
+    }
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] args ) {
+        if (sender instanceof Player p) {
+            try {
+                data.BankTop();
+                String[] names = data.getTopPlayers();
+                double[] balances = data.getTopBalances();
+                p.sendMessage(Component.text(" -----").color(NamedTextColor.YELLOW).append(Component.text(" Bank Top ").color(NamedTextColor.GOLD)).append(Component.text("-----").color(NamedTextColor.YELLOW)));
+                for (int i = 0; i < names.length; i++) {
+                    if (names[i] != null) {
+                        p.sendMessage((i + 1) + ". " + names[i] + ", $" + balances[i]);
+                    }
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        return List.of();
+    }
+}
