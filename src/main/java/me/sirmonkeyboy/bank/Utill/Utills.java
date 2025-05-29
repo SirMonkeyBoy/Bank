fa39bbe8e2587688440b0db3630fe76b30c28a65
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-05-29 17:24:37 +1000

    Adds StartBanner to Utills

--- src/main/java/me/sirmonkeyboy/bank/Utill/Utills.java
+++ src/main/java/me/sirmonkeyboy/bank/Utill/Utills.java
@@ -1,4 +1,22 @@
 package me.sirmonkeyboy.bank.Utill;
 
+import net.kyori.adventure.audience.Audience;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+
+import org.bukkit.Bukkit;
+
 public class Utills {
+
+    static Audience console = Bukkit.getConsoleSender();
+
+    public static void StartBanner() {
+        console.sendMessage(Component.text("########################################################################################################################"));
+        console.sendMessage(Component.text("#").append(Component.text("  #   #   ###   #       #    ####    #####      #####     #              #      ####      ##     #       #   #    #   ").color(NamedTextColor.GOLD)).append(Component.text("#")));
+        console.sendMessage(Component.text("#").append(Component.text("  #  #     #    # #     #   #        #    #    #     #    # #          # #      #   #    #  #    # #     #   #  #     ").color(NamedTextColor.GOLD)).append(Component.text("#")));
+        console.sendMessage(Component.text("#").append(Component.text("  ##       #    #   #   #   #   ##   #     #  #       #   #   #      #   #      ####    ######   #   #   #   ##       ").color(NamedTextColor.GOLD)).append(Component.text("#")));
+        console.sendMessage(Component.text("#").append(Component.text("  #  #     #    #     # #   #    #   #    #    #     #    #    #   #     #      #   #   #    #   #     # #   #  #     ").color(NamedTextColor.GOLD)).append(Component.text("#")));
+        console.sendMessage(Component.text("#").append(Component.text("  #   #   ###   #       #    ####    #####      #####     #      #       #      ####    #    #   #       #   #    #   ").color(NamedTextColor.GOLD)).append(Component.text("#")));
+        console.sendMessage(Component.text("########################################################################################################################"));
+    }
 }
