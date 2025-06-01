6798ae6b1143a43bd6b9b727940a35dd6805f083
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-06-01 11:14:33 +1000

    add /bank help
    just if players try /bank help not just /bank

--- src/main/java/me/sirmonkeyboy/bank/Commands/CommandManager.java
+++ src/main/java/me/sirmonkeyboy/bank/Commands/CommandManager.java
@@ -1,10 +1,7 @@
 package me.sirmonkeyboy.bank.Commands;
 
 import me.sirmonkeyboy.bank.Bank;
-import me.sirmonkeyboy.bank.Commands.SubCommands.Bal;
-import me.sirmonkeyboy.bank.Commands.SubCommands.Balance;
-import me.sirmonkeyboy.bank.Commands.SubCommands.Deposit;
-import me.sirmonkeyboy.bank.Commands.SubCommands.Withdraw;
+import me.sirmonkeyboy.bank.Commands.SubCommands.*;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -30,6 +27,7 @@ public class CommandManager implements TabExecutor {
         subcommands.add(new Deposit(plugin));
         subcommands.add(new Withdraw(plugin));
         subcommands.add(new Bal(plugin));
+        subcommands.add(new Help());
     }
 
     @Override
