9dc8d679d60c91abef68495701142c8445b81070
SirMonkeyBoy <116051515+SirMonkeyBoy@users.noreply.github.com>
2025-06-03 08:02:29 +1000

    Add logic to Reload

--- src/main/java/me/sirmonkeyboy/bank/Commands/ABankSubCommands/Reload.java
+++ src/main/java/me/sirmonkeyboy/bank/Commands/ABankSubCommands/Reload.java
@@ -1,4 +1,57 @@
 package me.sirmonkeyboy.bank.Commands.ABankSubCommands;
 
+import me.sirmonkeyboy.bank.Bank;
+import me.sirmonkeyboy.bank.Commands.SubCommand;
+import me.sirmonkeyboy.bank.Utils.ConfigManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class Reload extends SubCommand {
+
+    private final Bank bank;
+
+    private final ConfigManager configManager;
+
+    public Reload(Bank bank, ConfigManager configManager) {
+        this.bank = bank;
+        this.configManager = configManager;
+    }
+
+    @Override
+    public String getName() {
+        return "reload";
+    }
+
+    @Override
+    public String getDescription() {
+        return "reloads KingdomBank's config";
+    }
+
+    @Override
+    public String getSyntax() {
+        return "/abank reload";
+    }
+
+    @Override
+    public void perform(Player p, String[] args) {
+        if (!(p.hasPermission("Bank.commands.ABank.Reload"))) {
+            if (configManager.getNoPermission() == null || configManager.getNoPermission().isEmpty()){
+                p.sendMessage(Component.text(configManager.getMissingMessage()).color(NamedTextColor.RED));
+                return;
+            }
+            p.sendMessage(Component.text(configManager.getNoPermission()).color(NamedTextColor.RED));
+            return;
+        }
+
+        configManager.reloadConfigManager(p);
+    }
+
+    @Override
+    public List<String> getSubCommandArguments(Player p, String[] args) {
+        return List.of();
+    }
 }
